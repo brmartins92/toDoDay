1d525d127b5893693504397d5a80c34f73a9204f
BrunoSantosMartins <b.dos.santos.martins@accenture.com>
2023-02-25 18:51:20 -0300

    install tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
 import Habit from './components/Habit'
+import './styles/global.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <Habit completed={3}/>
+    <div>
+        <Habit completed={3}/>
+        <Habit completed={3}/>
+        <Habit completed={3}/>
+        <Habit completed={3}/>
+    </div>
   )
 }
 
